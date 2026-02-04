c9765c8a06d98e0230fdf8563431dc739508612a
Tiyasha Bhattacharjee <126256556+Tiyasha20@users.noreply.github.com>
2026-02-04 15:40:14 +0530

    Improve Responsive newscards

--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -1,12 +1,18 @@
 import image from '../assets/news.png'
 const Newsitem = ({title,description,src,url}) => {
   return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"305px"}}>
-  <img src={src?src:image} style={{height:"180px",width:"290px"}} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description?description.slice(0,90):""}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
+    <div className="card bg-dark text-light h-100 shadow" >
+  <img 
+  src={src?src:image} 
+  style={{ height: "180px", objectFit: "cover" }} 
+  className="card-img-top img-fluid" alt="..."/>
+  <div className="card-body d-flex flex-column">
+    <h5 className="card-title">{title ? title.slice(0, 50) : "No Title"}</h5>
+    <p className="card-text flex-grow-1">{description?description.slice(0,90):""}</p>
+    <a href={url}
+  target="_blank"
+  rel="noreferrer"
+  className="btn btn-primary mt-auto">Read More</a>
   </div>
 </div>
   )
