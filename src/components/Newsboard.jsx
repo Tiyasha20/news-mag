e802968e037cc5338aca33140870fe8cd3a6d943
Tiyasha Bhattacharjee <126256556+Tiyasha20@users.noreply.github.com>
2026-02-04 11:38:23 +0530

    Switch to GNews API

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -6,14 +6,14 @@ import Newsitem from "./Newsitem";
 const Newsboard = ({category}) => {
   const[articles,setArticles]=useState([]);
   useEffect(()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url=`https://gnews.io/api/v4/search?q=${category}&lang=en&max=5&apikey=${import.meta.env.VITE_API_KEY}`;
     fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
   },[category])
   return (
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
       {articles.map((news,index)=>{
-        return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+        return <Newsitem key={index} title={news.title} description={news.description} src={news.image} url={news.url}/>
       })}
     </div>
   )
